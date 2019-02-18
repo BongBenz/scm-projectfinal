ce1769e9dfb97cf48237672158c87f85c97e4e72
BongBenz <BongBenzRec@Gmail.com>
2019-02-18 09:55:07 +0700

    Add directory empSalary

---
+++ empSalary/src/empSalary/DemoProject.java
@@ -0,0 +1,10 @@
+package empSalary;
+
+public class DemoProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ empSalary/src/empSalary/inputProject.java
@@ -0,0 +1,5 @@
+package empSalary;
+
+public class inputProject {
+
+}
